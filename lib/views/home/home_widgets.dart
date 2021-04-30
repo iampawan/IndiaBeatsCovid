ec31bf87982ba9368e773e5f6885d00333182b8f
iampawan <mtechviral@gmail.com>
2021-04-30 09:45:51 +0530

    Bug Fixes

--- lib/views/home/home_widgets.dart
+++ lib/views/home/home_widgets.dart
@@ -32,29 +32,32 @@ class HomeWidgets extends StatelessWidget {
         10.heightBox,
         "Version - ${Constants.appVersion}".text.caption(context).make(),
         10.heightBox,
-        Link(
-          uri: Uri.parse(Constants.devUrl),
-          target: LinkTarget.blank,
-          builder: (context, followLink) => "Reach to developer"
-              .text
-              .rose400
-              .underline
-              .semiBold
-              .make()
-              .onTap(followLink),
-        ),
-        10.heightBox,
-        Link(
-          uri: Uri.parse(Constants.githubUrl),
-          target: LinkTarget.blank,
-          builder: (context, followLink) => "Check on github"
-              .text
-              .teal400
-              .underline
-              .semiBold
-              .make()
-              .onTap(followLink),
-        ),
+        Wrap(spacing: 10.0, runSpacing: 10.0, children: [
+          Link(
+              uri: Uri.parse(Constants.devUrl),
+              target: LinkTarget.blank,
+              builder: (context, followLink) => ElevatedButton(
+                    onPressed: followLink,
+                    child: "Reach to developer".text.make(),
+                  )),
+          10.heightBox,
+          Link(
+              uri: Uri.parse(Constants.githubUrl),
+              target: LinkTarget.blank,
+              builder: (context, followLink) => ElevatedButton(
+                    onPressed: followLink,
+                    child: "Check on github".text.make(),
+                  )),
+          10.heightBox,
+          Link(
+              uri:
+                  Uri.parse(store?.versionChecker?.link ?? Constants.githubUrl),
+              target: LinkTarget.blank,
+              builder: (context, followLink) => ElevatedButton(
+                    onPressed: followLink,
+                    child: "Download APK".text.make(),
+                  )),
+        ]),
       ].vStack(crossAlignment: CrossAxisAlignment.start).p16()
     ].vStack(crossAlignment: CrossAxisAlignment.start);
   }
